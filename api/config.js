4bf2349bba70d1e3ba32409e8f3312a51295b5e7
German Golder <111072926+ggolder22@users.noreply.github.com>
2026-07-11 17:13:01 -0300

    se crear el config.js dentro de la ruta de api

--- api/config.js
+++ api/config.js
@@ -1,1 +1,11 @@
+export default function handler(req, res) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET');
 
+  res.status(200).json({
+    host:     process.env.MQTT_HOST,
+    port:     parseInt(process.env.MQTT_PORT || '8884'),
+    username: process.env.MQTT_USER,
+    password: process.env.MQTT_PASSWORD
+  });
+}
