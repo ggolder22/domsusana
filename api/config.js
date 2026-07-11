4fd5154ba3f3254b911b8a0e7750671208a01256
German Golder <111072926+ggolder22@users.noreply.github.com>
2026-07-11 17:39:12 -0300

    harcodeo del api/config

--- api/config.js
+++ api/config.js
@@ -1,9 +1,11 @@
 module.exports = function handler(req, res) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET');
+
   res.status(200).json({
-    host:     process.env.MQTT_HOST,
-    port:     process.env.MQTT_PORT,
-    username: process.env.MQTT_USER,
-    password: process.env.MQTT_PASSWORD,
-    all_keys: Object.keys(process.env)
+    host:     'dc0ae886b9c7482695c794c9ca02717e.s1.eu.hivemq.cloud',
+    port:     8884,
+    username: 'domsusana',
+    password: 'Eciiii2580#'
   });
 };
